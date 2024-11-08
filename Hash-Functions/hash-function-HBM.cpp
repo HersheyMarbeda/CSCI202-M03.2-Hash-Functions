63208f836232b9e9f8f63c41510c4cf8e3e29aa6
Hershey Marbeda <126095800+HersheyMarbeda@users.noreply.github.com>
2024-11-08 15:31:42 -0500

    Update hash-function-HBM.cpp

--- Hash-Functions/hash-function-HBM.cpp
+++ Hash-Functions/hash-function-HBM.cpp
@@ -1,26 +1,25 @@
-/*
-    Programming Assignment: M03.2: Hash Table
-    File: hash-function-HBM.cpp
-    
-    Programmer: Hershey Marbeda
-    Instructor: Dr. Carver
-    Course: CSCI 202: Data Structures and Algorithms
-    College: Ivy Tech Community College
-    Due Date: 11.12.2024
- 
-    ------------About the Program:--------------
-    Program Purpose:
-    The program demonstrates the use of hash table with three different hash functions.
- 
- 
- 
- Assignment:
-    Write definition of search, isItemAtEqual, retrieve, remove, print, constructor, and destructor
-    for class hashT based upon a method of your choice from the powerpoint or otherwise.
-    Write a test program to test with at least 3 different hashing functions.
- */
-
-
+/***************************************************************************************************************
+ *   Programming Assignment: M03.2: Hash Table                                                                 *
+ *   File: hash-function-HBM.cpp                                                                               *
+ *                                                                                                             *
+ *   Programmer: Hershey Marbeda                                                                               *
+ *   Instructor: Dr. Carver                                                                                    *
+ *   Course: CSCI 202: Data Structures and Algorithms                                                          *
+ *   College: Ivy Tech Community College                                                                       *
+ *   Due Date: 11.12.2024                                                                                      *
+ *                                                                                                             *
+ *   -------------------------------------About the Program:-------------------------------------------------  *
+ *   Program Purpose:                                                                                          *
+ *   The program demonstrates using a hash table with three different hash functions.                          *
+ *   Last Date Updated: 11.8.2024                                                                              *
+ *                                                                                                             *
+ *                                                                                                             *
+ *   Assignment:                                                                                               *
+ *       Write definition of search, isItemAtEqual, retrieve, remove, print, constructor, and destructor       *
+ *       for class hashT based upon a method of your choice from the PowerPoint or otherwise.                  *
+ *       Write a test program to test with at least 3 different hashing functions.                             *
+ *                                                                                                             *
+ ***************************************************************************************************************/
 
 #include <iostream>
 #include <functional> // to use hash function
@@ -40,6 +39,12 @@ size_t custom_hash(const string& str)
 
 int main()
 {
+    // Table Heading
+    cout << string (100, '-') << endl
+         << "\t\t\t\t\t\t\t\t\tHash Function Demonstration" << endl
+         << string (100, '-') << endl << endl;
+    
+    // Example Hash
     cout << "Example Hash: " << custom_hash("Hello World") << endl << endl;
     return 0;
 }
